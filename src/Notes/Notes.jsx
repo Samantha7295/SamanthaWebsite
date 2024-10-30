637979f253c8d853b6e191bf8d4fb57dd6272976
Samantha7295 <124851974+Samantha7295@users.noreply.github.com>
2024-10-29 22:28:05 -0400

    fourth commit

--- src/Notes/Notes.jsx
+++ src/Notes/Notes.jsx
@@ -1,7 +1,4 @@
 // Tutorial Referenced: https://www.youtube.com/watch?v=WbV3zRgpw_E&list=PLjwm_8O3suyMMs7kfDD-p-yIhlmEgJkDj&index=7
 /* To do
- 1. Change all the arrows to better png pics
- 2. Fix up projects
- 3. Fix where the farmer logo is
- 4. Deal with git
+ 1. Deal with git
 */
