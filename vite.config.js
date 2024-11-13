8255469080365de46f4b821ec7e4b16f4605787e
Samantha7295 <124851974+Samantha7295@users.noreply.github.com>
2024-11-13 15:37:18 -0500

    seventeenth commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/samantha7295.github.io/', // Optional, as this is the default value
+  outDir: 'build',
+  base: '/', // Optional, as this is the default value
 })
