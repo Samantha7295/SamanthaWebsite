af661b3958218b3a886440de2060882396504e7b
Samantha7295 <124851974+Samantha7295@users.noreply.github.com>
2024-11-13 16:15:39 -0500

    Changed base "/" to "./"

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/SamanthaWebsite",
+  base: "./",
   plugins: [react()] // Optional, as this is the default value
 })
