626208455f29376b1165e88f5a516749df55ab3d
Samantha7295 <124851974+Samantha7295@users.noreply.github.com>
2024-11-13 15:43:24 -0500

    Added "build" to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  outDir: 'build',
   base: '/', // Optional, as this is the default value
+  build: {
+    outDir: 'build'
+  }
 })
