48c589dca6df3b26e9d5b942bbba48603400a2c7
Samantha7295 <124851974+Samantha7295@users.noreply.github.com>
2024-11-13 16:47:25 -0500

    Reset a bunch of files and folders

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",
-  plugins: [react()] // Optional, as this is the default value
+  // base: "./",
+  plugins: [react()], // Optional, as this is the default value
+  base: "/SamanthaWebsite/"
 })
