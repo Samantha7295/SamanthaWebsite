c7808be1fcdb525fc92d6492980b808477656bb0
Samantha7295 <124851974+Samantha7295@users.noreply.github.com>
2024-12-03 21:18:54 -0500

    Added Freakmometer to Projects

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import './Projects.css'
 import project_1 from '../../assets/project-1.png'
-import project_2 from '../../assets/project-2.png'
+import project_2 from '../../assets/devpost_thumbnail.png'
 import project_3 from '../../assets/project-3.png'
 import project_icon_1 from '../../assets/project-icon-1.png'
 import project_icon_2 from '../../assets/project-icon-2.png'
 import project_icon_3 from '../../assets/project-icon-3.png'
-
 
 const Projects = () => {
   return (
@@ -23,7 +22,8 @@ const Projects = () => {
             <img src={project_2} alt=""/>
             <div className="caption">
                 <img src={project_icon_2} alt="" />
-                <h3>Second Project</h3>
+                <h3>FREAKMOMETER</h3>
+                <p>FREAKMOMETER is a 5-day weather app built with TypeScript, React, and Tailwind CSS, designed to deliver a dynamic "FREAKCAST" alongside a unique, humorous user experience. It features API integrations to fetch and display weather data, adapting from traditional CSS to Tailwind for streamlined styling. </p>
             </div>
         </div>
         <div className="project"> 
